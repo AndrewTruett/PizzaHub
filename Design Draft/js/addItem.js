88d17d14b74910e96427ce09e006a10cd106d25a
AndrewTruett <andrewtruett@aol.com>
2018-04-22 21:09:22 -0400

    Popular item add button now adds item to shopping cart.

---
+++ Design Draft/js/addItem.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#add-pop-item-btn").click(function(){
+        var order = $("#pop-item-row a.active").text();
+        if(order == "")
+            alert("Please make a selection.");
+        else
+        $(".list-group-flush").append('<li class="list-group-item">'+ order +'</li>');
+      })
+    
+    
+    
+    $("a").on("click", function(e){
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    })
+    
+  });
