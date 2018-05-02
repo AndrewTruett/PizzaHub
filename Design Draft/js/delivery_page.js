6b81fbeec5afa6154d2290c198c62bebfb84393f
Shofiqur Rahman <shofi@Shofiqurs-MacBook-Pro.local>
2018-05-01 23:11:23 -0700

    updating html file

--- Design Draft/js/delivery_page.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-   $(".list-group-item").on("click", function() {
-        $(this).toggleClass("active");
-        $(this).siblings().removeClass("active");
-   });
-});
