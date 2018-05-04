2d055c7c36a0f9c5140d2b1c059107724b52e505
Andrew Truett <andrewtruett@aol.com>
2018-05-04 01:35:44 -0400

    Removed duplicate files and folders.

--- Design Draft/js/create_account.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    
-    $("#create-account-btn").click(function() {
-        var currentStore = localStorage.getItem("currentStore").toLowerCase().trim().split(" ").join("_");//converts store name to lower case with underscores instead of spaces
-        
-    });
-});
