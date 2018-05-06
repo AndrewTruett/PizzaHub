5769e6749f946dc1a6bd7c42b262f671533ae75c
Andrew Truett <andrewtruett@aol.com>
2018-05-06 17:54:53 -0400

    Added sample code for writing to files.

--- Design Draft/express_test.js
+++ Design Draft/express_test.js
@@ -1,16 +1,25 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var fs = require('fs');
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {//when user goes to domain.com/ this function is fired
-    //res.send('Home page');
     res.sendFile(__dirname+'/public/index.html');
 });
 app.get('/home', function(req, res) {//when user goes to domain.com/home this function is fired
     //res.send('Home page via /home');
     //res.sendFile("../../index.html");
     res.sendFile(__dirname+'/public/index.html');
+    
+    /*******This is how to write to a text file.
+    fs.writeFile(__dirname+'/public/system/test.txt', 'Hey there!', function(err) {
+        if(err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });*/
 });
 
 app.get('/store/:name', function(req, res) {
