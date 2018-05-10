4f253e826c1fe70646408ee333df49f729937a37
Farhan <fmshyder@gmail.com>
2018-05-09 20:11:57 -0400

    added function: checkLoginCustomer

--- Design Draft/public/js/readWrite.js
+++ Design Draft/public/js/readWrite.js
@@ -1,5 +1,24 @@
 var fs = require("fs");
-console.log("\n *STARTING* \n");
-var contents = fs.readFileSync("../system/customers.json");
-var data = JSON.parse(contents);
-console.log("User Name:",data.customers[0].memID);
+// console.log("\n *STARTING* \n");
+// var contents = fs.readFileSync("../system/customers.json");
+// var data = JSON.parse(contents);
+// console.log("User Name:",data.customers[0].memID);
+function checkLoginCustomer(username,password)
+{
+  var contents = fs.readFileSync("../system/customers.json");
+  var data = JSON.parse(contents);
+
+  for(var i=0; i<data.customers.length; i++)
+  {
+    if (data.customers[i].username == username)
+    {
+      if (data.customers[i].password == password)
+      {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+console.log(checkLoginCustomer("mickey","mouse"));
