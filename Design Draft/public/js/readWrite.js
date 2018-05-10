1f3c90f2e61ab48cfef92f60156f5ff0a61ae6c2
Farhan <fmshyder@gmail.com>
2018-05-09 21:30:08 -0400

    function added: checkUserType

--- Design Draft/public/js/readWrite.js
+++ Design Draft/public/js/readWrite.js
@@ -31,3 +31,30 @@ function checkLoginCustomer(username,password,store)
 }
 
 // console.log(checkLoginCustomer("mickey","mouse","littleItaly"));
+
+
+function checkUserType(username,store)
+//returns visitor if user not found for store
+{
+  var contents = fs.readFileSync("../system/customers.json");
+  var data = JSON.parse(contents);
+
+  for(var i=0; i<data.customers.length; i++)
+  {
+    if (data.customers[i].username == username)
+    {
+      for (var j = 0; j < data.customers[i].membership.length; j++)
+      {
+        if (data.customers[i].membership[j].store == store)
+        {
+          return data.customers[i].membership[j].type;
+        }
+      }
+    }
+  }
+
+  return "visitor";
+}
+// console.log(checkUserType("mickey","littleItaly"));
+// console.log(checkUserType("mickey","garlicNewYorkPizzaBar"));
+// console.log(checkUserType("mickey","donHyder"));
