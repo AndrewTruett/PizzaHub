3acffb46a8e5929f81e057ecdf77a781fbaa067a
Farhan <fmshyder@gmail.com>
2018-05-10 12:24:16 -0400

    added: function checkCustomerRating(username,store)

--- Design Draft/public/js/readWrite.js
+++ Design Draft/public/js/readWrite.js
@@ -55,8 +55,35 @@ function checkCustomerType(username,store)
 
   return "visitor";
 }
-
-
 // console.log(checkCustomerType("mickey","littleItaly"));
 // console.log(checkCustomerType("mickey","garlicNewYorkPizzaBar"));
 // console.log(checkCustomerType("mickey","donHyder"));
+
+
+function checkCustomerRating(username,store)
+//pre-cond: customer is already a member of the store
+//post-cond: returns rating, returns 0 if not a member of the store
+{
+  var contents = fs.readFileSync("../system/customers.json");
+  var data = JSON.parse(contents);
+
+  for(var i=0; i<data.customers.length; i++)
+  {
+    if (data.customers[i].username == username)
+    {
+      for (var j = 0; j < data.customers[i].membership.length; j++)
+      {
+        if (data.customers[i].membership[j].store == store)
+        {
+          return data.customers[i].membership[j].rating;
+        }
+      }
+    }
+  }
+
+  return 0;
+}
+
+// console.log(checkCustomerRating("mickey","littleItaly"));
+// console.log(checkCustomerRating("mickey","garlicNewYorkPizzaBar"));
+// console.log(checkCustomerRating("mickey","donHyder"));
