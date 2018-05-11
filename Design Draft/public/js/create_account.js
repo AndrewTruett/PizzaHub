08c9c2d1341219548d8e512f33f3d56ba9a75bb9
Farhan <fmshyder@gmail.com>
2018-05-11 07:21:11 -0400

    photo url updated

--- Design Draft/public/js/create_account.js
+++ Design Draft/public/js/create_account.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-    
+
     $("#create-account-btn").click(function() {
         var currentStore = localStorage.getItem("currentStore").toLowerCase().trim().split(" ").join("_");//converts store name to lower case with underscores instead of spaces
-        
+
         try {
             var username = $("#username").val();
             var password = $("#password").val();
-            xmlHttp.open("GET", currentStore+"/newuser/"+username+"/"+password, true);
+            var photoUrl = $("#photoUrl").val();
+            xmlHttp.open("GET", currentStore+"/newuser/"+username+"/"+password+"/"+photoUrl, true);
             xmlHttp.onreadystatechange = handleServerResponse;
             xmlHttp.send(null);
-            
+
             window.location.href="store_page.html";//This will depend on the users position (ie manager, cust, dg, etc)
         } catch(e) {
             console.log(e.toString());
