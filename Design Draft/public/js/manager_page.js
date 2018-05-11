d4896fd184d2837507fc18e15aa1df70b4919b35
Andrew Truett <andrewtruett@aol.com>
2018-05-10 22:14:31 -0400

    Manager page successfuly requests customers.json.

--- Design Draft/public/js/manager_page.js
+++ Design Draft/public/js/manager_page.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-   $(".list-group-item").on("click", function() {
+    $(".list-group-item").on("click", function() {
         $(this).toggleClass("active");
         $(this).siblings().removeClass("active");
-   });
+    });
+    
+    var xmlHttp = new XMLHttpRequest();
+    
+    try{
+        xmlHttp.open("GET", "get/file/customers.json", true);
+        
+        xmlHttp.onreadystatechange = function() {
+            if(xmlHttp.readyState == 4) {
+                if(xmlHttp.status == 200) { //Everything went okay
+                    console.log(JSON.parse(xmlHttp.responseText));
+                } else if (xmlHttp.status == 404) {
+                    console.log("404 not found");
+                }
+            } 
+        };
+        
+        xmlHttp.send(null);
+        
+    } catch(e) {
+        console.log(e.toString());
+    }
 });
