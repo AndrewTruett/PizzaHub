d887a3197e59326572e4d3b699049d34f4f4fde2
Andrew Truett <andrewtruett@aol.com>
2018-05-11 01:07:42 -0400

    Readded manager_page.js.

--- Design Draft/public/js/manager_page.js
+++ Design Draft/public/js/manager_page.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     
     var xmlHttp = new XMLHttpRequest();
     
+    //Load customers
     try{
         xmlHttp.open("GET", "get/file/customers.json", true);
         
@@ -29,10 +30,36 @@ $(document).ready(function() {
                         }
                     }
                     
+                } else if (xmlHttp.status == 404) {
+                    console.log("404 not found");
+                }
+            } 
+        };
+        
+        xmlHttp.send(null);
+        
+    } catch(e) {
+        console.log(e.toString());
+    }
+    
+    //Load employees
+    try{
+        xmlHttp.open("GET", "get/file/cooks.json", true);
+        
+        xmlHttp.onreadystatechange = function() {
+            if(xmlHttp.readyState == 4) {
+                if(xmlHttp.status == 200) { //Everything went okay
+                    console.log(JSON.parse(xmlHttp.responseText));
+                    var json = JSON.parse(xmlHttp.responseText);
                     
+                    var currentStore = localStorage.getItem("currentStore").toLowerCase().trim().split(" ").join("_");//converts store name to lower case with underscores instead of spaces
                     
-                    
-                    
+                    for(var i = 0; i<json.cooks.length; i++) {
+                        var username = json.cooks[i].username;
+                        
+                        if(json.cooks[i].store == currentStore)//user has to be member of this store
+                                $("#employee-list").append('<tr><th><input type = "checkbox" name="record"></th><th>'+username+'</th><th>'+json.cooks[i].rating+'</th></tr>'); 
+                    }
                 } else if (xmlHttp.status == 404) {
                     console.log("404 not found");
                 }
