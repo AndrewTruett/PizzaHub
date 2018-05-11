1353f8a86242ef4096c7154495b3603e6f711933
Andrew Truett <andrewtruett@aol.com>
2018-05-11 06:23:18 -0400

    Manager can now send deliveries to delivery guys.

--- Design Draft/public/js/manager_page.js
+++ Design Draft/public/js/manager_page.js
@@ -2,10 +2,56 @@ $(document).ready(function() {
     
     $("#store-name").text(localStorage.getItem("currentStore"));
     
+    //Handling of active list items
+    $(document).on("click", "li", function(e){
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    });
+    
     $(".list-group-item").on("click", function() {
         $(this).toggleClass("active");
         $(this).siblings().removeClass("active");
     });
+    
+    checkForDeliveries();
+    
+    function checkForDeliveries() {
+        var xmlHttp = new XMLHttpRequest();
+
+        //load orders
+        try{
+            xmlHttp.open("GET", "get/file/orders.json", true);
+
+            xmlHttp.onreadystatechange = function() {
+                if(xmlHttp.readyState == 4) {
+                      if(xmlHttp.status == 200) { //Everything went okay
+                          console.log(JSON.parse(xmlHttp.responseText));
+                          var json = JSON.parse(xmlHttp.responseText);
+
+                          var currentStore = localStorage.getItem("currentStore").toLowerCase().trim().split(" ").join("_");//converts store name to lower case with underscores instead of spaces
+
+                          for(var i = 0; i<json.orders.length; i++)
+                          {
+                            if (json.orders[i].status == "cooked")
+                            {
+                                $("#ready-items").append('<li class="list-group-item">'+json.orders[i].customer+'<br><p class="sub-heading">Placed at:<div id="time">'+json.orders[i].time+'</div><br> Price: $'+json.orders[i].price+'</p></li>');
+                                                         
+                              }
+                            }
+                          }
+
+                      } else if (xmlHttp.status == 404) {
+                          console.log("404 not found");
+                      }
+          };
+
+      xmlHttp.send(null);
+      //xmlHttp.abort();
+
+  } catch(e) {
+      console.log(e.toString());
+  }
+    }
     
     var xmlHttp = new XMLHttpRequest();
     
@@ -91,8 +137,11 @@ $(document).ready(function() {
                     for(var i = 0; i<json.deliveryGuys.length; i++) {
                         var username = json.deliveryGuys[i].username;
                         
-                        if(json.deliveryGuys[i].store == currentStore && !json.deliveryGuys.laidOff)//user has to be member of this store
+                        if(json.deliveryGuys[i].store == currentStore && !json.deliveryGuys.laidOff) {//user has to be member of this store
                                 $("#employee-list").append('<tr><th><input type = "checkbox" name="record"></th><th>'+username+'</th><th>'+json.deliveryGuys[i].rating+'</th></tr>'); 
+                                
+                                $("#delivery-guys").append('<option value='+username+'>'+username+'</option>');
+                        }
                     }
                 } else if (xmlHttp3.status == 404) {
                     console.log("404 not found");
@@ -107,6 +156,35 @@ $(document).ready(function() {
     }
 });
 
-function populateLists(data) {
+function sendToDeliveryGuy() {
+    var time = $(".active #time").text();
+    console.log(time);
+    var deliveryGuy = $("#delivery-guys :selected").val();
+    console.log(deliveryGuy);
     
+    var xmlHttp2 = new XMLHttpRequest();
+
+  //load orders
+  try{
+      xmlHttp2.open("GET", "setDeliveryGuy/"+time+"/"+deliveryGuy, true);
+
+      xmlHttp2.onreadystatechange = function() {
+          if(xmlHttp2.readyState == 4) {
+              if(xmlHttp2.status == 200) { //Everything went okay
+                  console.log(JSON.parse(xmlHttp2.responseText));
+
+              } else if (xmlHttp2.status == 404) {
+                  console.log("404 not found");
+              }
+          }
+      };
+
+      xmlHttp2.send(null);
+
+  } catch(e) {
+      console.log(e.toString());
+  }
+    
+    //$("#active").remove();
+    document.location.reload(true);
 }
